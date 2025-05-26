b45bcaf229a8a1a4a332964b45f04a404665b6ce
haringk <haringk@gmail.com>
2025-05-26 13:10:01 +0200

    del

--- print_shop/print_shop/public/js/sales_order.js
+++
@@ -1,7 +0,0 @@
-frappe.ui.form.on('Sales Order', {
-    refresh: function(frm) {
-        frm.fields_dict.items.grid.add_custom_button(__('Add Optional'), function() {
-            frappe.msgprint(__('Implement optional selection here'));  
-        });
-    }
-});
